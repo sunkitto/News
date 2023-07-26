c17529b6f56fcb954c63264ad32933a33876735d
Sunkitto <platflow@gmail.com>
2023-07-26 03:13:04 +0200

    Implementation of NewsDao

---
+++ core/database/src/main/java/com/sunkitto/news/core/database/dao/NewsDao.kt
@@ -0,0 +1,17 @@
+package com.sunkitto.news.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.sunkitto.news.core.database.model.NewsEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NewsDao {
+
+    @Query("SELECT * FROM news ORDER BY published_at DESC")
+    fun getNews(): Flow<List<NewsEntity>>
+
+    @Upsert
+    suspend fun upsertNews(newsEntities: List<NewsEntity>)
+}
