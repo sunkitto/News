b3e32f50f495db4f5ba56876b5fe379e03cf0ae8
Sunkitto <platflow@gmail.com>
2023-07-28 02:38:54 +0200

    Add to NewsDatabase RecentSearchDao property

--- core/database/src/main/java/com/sunkitto/news/core/database/NewsDatabase.kt
+++ core/database/src/main/java/com/sunkitto/news/core/database/NewsDatabase.kt
@@ -5,10 +5,15 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.sunkitto.news.core.database.converters.InstantConverter
 import com.sunkitto.news.core.database.dao.NewsDao
+import com.sunkitto.news.core.database.dao.RecentSearchDao
 import com.sunkitto.news.core.database.model.NewsEntity
+import com.sunkitto.news.core.database.model.RecentSearchEntity
 
 @Database(
-    entities = [NewsEntity::class],
+    entities = [
+        NewsEntity::class,
+        RecentSearchEntity::class
+    ],
     version = 1
 )
 @TypeConverters(
@@ -17,4 +22,6 @@ import com.sunkitto.news.core.database.model.NewsEntity
 abstract class NewsDatabase : RoomDatabase() {
 
     abstract fun newsDao(): NewsDao
+
+    abstract fun recentSearchDao(): RecentSearchDao
 }
