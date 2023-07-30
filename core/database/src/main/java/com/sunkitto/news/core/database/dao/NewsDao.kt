0d4f6e2aafab1c300bde5b91f96185823a77cd62
Sunkitto <platflow@gmail.com>
2023-07-30 21:22:50 +0200

    Add query for searching of news

--- core/database/src/main/java/com/sunkitto/news/core/database/dao/NewsDao.kt
+++ core/database/src/main/java/com/sunkitto/news/core/database/dao/NewsDao.kt
@@ -12,6 +12,9 @@ interface NewsDao {
     @Query("SELECT * FROM news ORDER BY published_at DESC")
     fun getNews(): Flow<List<NewsEntity>>
 
+    @Query("SELECT * FROM news WHERE title LIKE :query OR description LIKE :query")
+    fun searchNews(query: String): Flow<List<NewsEntity>>
+
     @Upsert
     suspend fun upsertNews(newsEntities: List<NewsEntity>)
 }
