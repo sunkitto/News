6c0b1c9692af11266784dabf8cd5bbabac64ee8b
Sunkitto <platflow@gmail.com>
2023-08-02 20:27:22 +0200

    Implementation of SettingsPreferencesSerializer

---
+++ core/datastore/src/main/java/com/sunkitto/news/core/datastore/SettingsPreferencesSerializer.kt
@@ -0,0 +1,34 @@
+package com.sunkitto.news.core.datastore
+
+import androidx.datastore.core.Serializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+object SettingsPreferencesSerializer : Serializer<SettingsPreferences> {
+
+    override val defaultValue: SettingsPreferences
+        get() = SettingsPreferences()
+
+    override suspend fun readFrom(input: InputStream): SettingsPreferences {
+        return try {
+            Json.decodeFromString(
+                deserializer = SettingsPreferences.serializer(),
+                string = input.readBytes().decodeToString()
+            )
+        } catch(e: SerializationException) {
+            e.printStackTrace()
+            defaultValue
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun writeTo(t: SettingsPreferences, output: OutputStream) =
+        output.write(
+            Json.encodeToString(
+                serializer = SettingsPreferences.serializer(),
+                value = t
+            ).encodeToByteArray()
+        )
+}
