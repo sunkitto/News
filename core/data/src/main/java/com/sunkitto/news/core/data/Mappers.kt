d6b6678cd4a108ec2b9e5777a799122c7fd5e56b
Sunkitto <platflow@gmail.com>
2023-08-07 14:17:58 +0200

    Implementation of SettingsRepository

--- core/data/src/main/java/com/sunkitto/news/core/data/Mappers.kt
+++ core/data/src/main/java/com/sunkitto/news/core/data/Mappers.kt
@@ -1,7 +1,11 @@
 package com.sunkitto.news.core.data
 
 import com.sunkitto.news.core.database.model.ArticleEntity
+import com.sunkitto.news.core.database.model.RecentSearchEntity
 import com.sunkitto.news.core.database.model.SourceEntity
+import com.sunkitto.news.core.datastore.SettingsPreferences
+import com.sunkitto.news.core.model.RecentSearch
+import com.sunkitto.news.core.model.settings.Settings
 import com.sunkitto.news.core.network.model.ArticleDto
 
 fun ArticleDto.asArticleEntity() =
@@ -19,4 +23,23 @@ fun ArticleDto.asArticleEntity() =
         publishedAt = this.publishedAt,
         content = this.content,
         isHeadline = false,
+    )
+
+fun RecentSearchEntity.asRecentSearch() =
+    RecentSearch(
+        query = this.query,
+        date = this.date,
+    )
+
+fun RecentSearch.asRecentSearchEntity() =
+    RecentSearchEntity(
+        query = this.query,
+        date = this.date,
+    )
+
+fun SettingsPreferences.asSettings() =
+    Settings(
+        language = this.language,
+        theme = this.theme,
+        topHeadlinesCountry = topHeadlinesCountry,
     )
