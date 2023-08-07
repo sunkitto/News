fd28a805413e392e51d4209117eaffc7e7e72ad4
Sunkitto <platflow@gmail.com>
2023-08-07 15:04:48 +0200

    Implementation of TopHeadlinesUseCase

--- core/domain/src/main/java/com/sunkitto/news/core/domain/GetTopHeadlinesUseCase.kt
+++ core/domain/src/main/java/com/sunkitto/news/core/domain/GetTopHeadlinesUseCase.kt
@@ -1,4 +1,21 @@
 package com.sunkitto.news.core.domain
 
-class GetTopHeadlinesUseCase {
+import com.sunkitto.news.core.domain.repository.NewsRepository
+import com.sunkitto.news.core.domain.repository.SettingsRepository
+import com.sunkitto.news.core.model.TopHeadlinesCategory
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class GetTopHeadlinesUseCase @Inject constructor(
+    private val newsRepository: NewsRepository,
+    private val settingsRepository: SettingsRepository,
+) {
+
+    suspend operator fun invoke(category: TopHeadlinesCategory) {
+        val settings = settingsRepository.settings.first()
+        newsRepository.getTopHeadlines(
+            country = settings.topHeadlinesCountry,
+            category = category
+        )
+    }
 }
