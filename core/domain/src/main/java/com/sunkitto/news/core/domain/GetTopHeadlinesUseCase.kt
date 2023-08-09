287a80a9d55853308995d36fddf53813625a8c7f
Sunkitto <platflow@gmail.com>
2023-08-09 16:04:29 +0200

    Refactor of domain module

--- core/domain/src/main/java/com/sunkitto/news/core/domain/GetTopHeadlinesUseCase.kt
+++ core/domain/src/main/java/com/sunkitto/news/core/domain/GetTopHeadlinesUseCase.kt
@@ -1,8 +1,11 @@
 package com.sunkitto.news.core.domain
 
+import androidx.paging.PagingData
 import com.sunkitto.news.core.domain.repository.NewsRepository
 import com.sunkitto.news.core.domain.repository.SettingsRepository
+import com.sunkitto.news.core.model.Article
 import com.sunkitto.news.core.model.TopHeadlinesCategory
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
@@ -11,9 +14,9 @@ class GetTopHeadlinesUseCase @Inject constructor(
     private val settingsRepository: SettingsRepository,
 ) {
 
-    suspend operator fun invoke(category: TopHeadlinesCategory) {
+    suspend operator fun invoke(category: TopHeadlinesCategory): Flow<PagingData<Article>> {
         val settings = settingsRepository.settings.first()
-        newsRepository.getTopHeadlines(
+        return newsRepository.getTopHeadlines(
             country = settings.topHeadlinesCountry,
             category = category
         )
--- core/domain/src/main/java/com/sunkitto/news/core/domain/repository/NewsRepository.kt
+++ core/domain/src/main/java/com/sunkitto/news/core/domain/repository/NewsRepository.kt
@@ -1,7 +1,7 @@
 package com.sunkitto.news.core.domain.repository
 
 import androidx.paging.PagingData
-import com.sunkitto.news.core.database.model.ArticleEntity
+import com.sunkitto.news.core.model.Article
 import com.sunkitto.news.core.model.TopHeadlinesCategory
 import com.sunkitto.news.core.model.settings.TopHeadlinesCountry
 import kotlinx.coroutines.flow.Flow
@@ -11,7 +11,7 @@ interface NewsRepository {
     fun getTopHeadlines(
         country: TopHeadlinesCountry,
         category: TopHeadlinesCategory,
-    ): Flow<PagingData<ArticleEntity>>
+    ): Flow<PagingData<Article>>
 
-    fun getAllNews(query: String): Flow<PagingData<ArticleEntity>>
+    fun getAllNews(query: String): Flow<PagingData<Article>>
 }
