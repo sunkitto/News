9ae631a40c6e69aa7fea41f62383f8e6372c168f
Sunkitto <platflow@gmail.com>
2023-08-09 16:09:51 +0200

    Add DateTimeFormat with appropriate unit test for it

---
+++ util/src/main/java/com/sunkitto/news/util/DateTimeFormat.kt
@@ -0,0 +1,13 @@
+package com.sunkitto.news.util
+
+import kotlinx.datetime.Instant
+import kotlinx.datetime.toJavaInstant
+
+/**
+ * Returns localized time in short format like: 08.08.2023, 18:24
+ */
+fun Instant.toLocalizedDateTimeString(): String {
+    val zonedTime = java.time.ZonedDateTime.ofInstant(this.toJavaInstant(), java.time.ZoneId.systemDefault())
+    val formatter = java.time.format.DateTimeFormatter.ofLocalizedDateTime(java.time.format.FormatStyle.SHORT)
+    return zonedTime.format(formatter)
+}
