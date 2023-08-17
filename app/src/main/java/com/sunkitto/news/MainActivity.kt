c90051b1622da561bb2f853febbf7933c52833ed
Sunkitto <platflow@gmail.com>
2023-08-17 18:11:44 +0200

    Implementation of Bottom Navigation

--- app/src/main/java/com/sunkitto/news/MainActivity.kt
+++ app/src/main/java/com/sunkitto/news/MainActivity.kt
@@ -2,10 +2,44 @@ package com.sunkitto.news
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.sunkitto.news.core.datastore.model.SettingsPreferences
+import com.sunkitto.news.core.model.settings.Theme
+import com.sunkitto.news.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var dataStore: DataStore<SettingsPreferences>
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        applicationContext.appComponent.inject(this)
+
+        lifecycleScope.launch {
+            val settings = dataStore.data.first()
+            when (settings.theme) {
+                Theme.FOLLOW_SYSTEM -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                Theme.LIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Theme.DARK -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
     }
 }
