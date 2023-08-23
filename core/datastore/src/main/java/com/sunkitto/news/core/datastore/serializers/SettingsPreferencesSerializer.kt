2ab423f6c7477916963b81f8318968ae708a311c
Sunkitto <platflow@gmail.com>
2023-08-23 17:42:28 +0200

    Test SettingsPreferencesSerializer in core.datastore module

--- core/datastore/src/main/java/com/sunkitto/news/core/datastore/serializers/SettingsPreferencesSerializer.kt
+++ core/datastore/src/main/java/com/sunkitto/news/core/datastore/serializers/SettingsPreferencesSerializer.kt
@@ -2,10 +2,12 @@ package com.sunkitto.news.core.datastore.serializers
 
 import androidx.datastore.core.Serializer
 import com.sunkitto.news.core.datastore.model.SettingsPreferences
-import java.io.InputStream
-import java.io.OutputStream
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
 
 object SettingsPreferencesSerializer : Serializer<SettingsPreferences> {
 
@@ -24,12 +26,13 @@ object SettingsPreferencesSerializer : Serializer<SettingsPreferences> {
         }
     }
 
-    @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun writeTo(t: SettingsPreferences, output: OutputStream) =
-        output.write(
-            Json.encodeToString(
-                serializer = SettingsPreferences.serializer(),
-                value = t,
-            ).encodeToByteArray(),
-        )
+        withContext(Dispatchers.IO) {
+            output.write(
+                Json.encodeToString(
+                    serializer = SettingsPreferences.serializer(),
+                    value = t,
+                ).encodeToByteArray(),
+            )
+        }
 }
---
+++ core/datastore/src/test/java/com/sunkitto/news/core/datastore/SettingsPreferencesSerializerTest.kt
@@ -0,0 +1,34 @@
+package com.sunkitto.news.core.datastore
+
+import com.sunkitto.news.core.datastore.model.SettingsPreferences
+import com.sunkitto.news.core.datastore.serializers.SettingsPreferencesSerializer
+import com.sunkitto.news.core.model.settings.Language
+import com.sunkitto.news.core.model.settings.Theme
+import com.sunkitto.news.core.model.settings.TopHeadlinesCountry
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class SettingsPreferencesSerializerTest {
+
+    @Test
+    fun settings_preferences_serialize_and_deserialize() = runTest {
+
+        val serializer = SettingsPreferencesSerializer
+        val output = ByteArrayOutputStream()
+
+        val settingsPreferences = SettingsPreferences(
+            language = Language.ENGLISH,
+            theme = Theme.DARK,
+            topHeadlinesCountry = TopHeadlinesCountry.ARGENTINA,
+        )
+        serializer.writeTo(settingsPreferences, output)
+
+        val inputStream = ByteArrayInputStream(output.toByteArray())
+        val actualSettings = serializer.readFrom(inputStream)
+
+        assertEquals(settingsPreferences, actualSettings)
+    }
+}
