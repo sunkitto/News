c5bde132ec6bedbe761704710ac8e32f6cd37039
Sunkitto <platflow@gmail.com>
2023-08-24 13:47:32 +0200

    Remove penaltyDeath when StrictMode violation appeared in VM policy

--- app/src/main/java/com/sunkitto/news/NewsApp.kt
+++ app/src/main/java/com/sunkitto/news/NewsApp.kt
@@ -49,7 +49,6 @@ class NewsApp : Application() {
                     .detectLeakedSqlLiteObjects()
                     .detectLeakedClosableObjects()
                     .penaltyLog()
-                    .penaltyDeath()
                     .build(),
             )
         }
